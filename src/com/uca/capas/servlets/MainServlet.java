ab128e82e9bc9b098b2dd7bf973d583a07c85b9f
dagr27 <00118717@uca.edu.sv>
2020-03-28 18:26:10 -0600

    Subiendo Tarea del laboratorio 1

---
+++ src/com/uca/capas/servlets/MainServlet.java
@@ -0,0 +1,57 @@
+package com.uca.capas.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+		String user, pass, validUser, validPass;
+		PrintWriter salida = response.getWriter();
+		validUser = "UCA";
+		validPass = "12345";
+		user = request.getParameter("user");
+		pass = request.getParameter("pass");
+		if(user.equals(validUser) && pass.equals(validPass)) {
+			salida.println("<span><b>ACCESO CORRECTO</b><span>");
+		} else {
+			salida.println("<span><b>ACCESO INCORRECTO</b><span>");
+		}
+	}
+
+}
